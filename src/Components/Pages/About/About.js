c21eb5a69add159312857d542991517a46acad47
MdMehedi-Hasan <mh19115@gmail.com>
2022-04-18 23:34:11 +0600

    final commit

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -3,13 +3,24 @@ import image from '../../../images/mehedi.png'
 
 const About = () => {
     return (
-        <div className=''>
-            <div className=''>
-                <img style={{borderRadius:"50%", width:"20%", backgroundColor:"#41464b75"}} src={image} alt="" />
+        <div className='row'>
+            <div className='col-6 d-flex justify-content-center'>
+                <img style={{borderRadius:"50%", backgroundColor:"#41464b75"}} src={image} alt="" />
             </div>
-            <div>
+            <div className='col-6'>
                 <h1>Hi, I'm Md. Mehedi Hasan</h1>
-                <p></p>
+                <p>I was trying to learn coding by myself early 2019. But everytime javascript pulled me back. When i realised i can't learn it by myself i jumped into this course. The primary goal is to learn basic structure of web (How web works, Programming languages,Interactions etc).</p>
+                <h1>Professional goal</h1>
+                <ul>
+                    <h5>I would like to learn these technologies in coming years</h5>
+                    <li>Python</li>
+                    <li>Flutter</li>
+                    <li>Django</li>
+                    <li>Angular</li>
+                    <li>Algorithm</li>
+                    <li>Data structure</li>
+                </ul>
+                <p>However, the final goal is to run my own it farm. So that i can work independently and could create new employment for others.</p>
             </div>
         </div>
     );
